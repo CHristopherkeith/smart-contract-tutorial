d284e4c20743b61a3519e260e5b8ff92c6b533a5
Christopher <easonmu@hotmail.com>
2018-03-21 00:15:16 +0800

    truffle with node

--- external.js
+++ external.js
@@ -1,5 +1,9 @@
-module.exports = function(callback) {
-  // perform actions
-  console.log(callback)
-  console.log("2333")
-}
+// module.exports = function(callback) {
+//   // perform actions
+//   console.log(callback)
+//   console.log("2333")
+// }
+// //////////////////////////////////////
+var provider = new Web3.providers.HttpProvider("http://localhost:8545");
+// //////////////////////////////////////
+// console.log('2333')

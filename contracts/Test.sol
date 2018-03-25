ae65cc3791e49011b0b77a80d416ca3e06a3f8cc
Christopher <easonmu@hotmail.com>
2018-03-25 22:13:58 +0800

    constract test

---
+++ contracts/Test.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.17;
+
+contract Test{
+
+    function f() public returns (string) {
+        return "method f()";
+    }
+
+    function g() public returns (string) {
+        return "method g()";
+    }
+}
